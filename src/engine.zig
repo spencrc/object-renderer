256a534d03c9773ac420792cf3c0f334dda2a950
spencrc <145073726+spencrc@users.noreply.github.com>
2026-08-03 20:47:08 -0700

    feat: added debug messenger

--- src/engine.zig
+++ src/engine.zig
@@ -95,21 +95,21 @@ fn initVulkan(self: *Engine) !void {
     self.instance = vk.InstanceProxy.init(instance, vki);
     errdefer self.instance.destroyInstance(null);
 
-    // if (builtin.mode == .Debug) {
-    //     self.debug_messenger = try self.instance.createDebugUtilsMessengerEXT(&.{
-    //         .message_severity = .{
-    //             .warning_bit_ext = true,
-    //             .error_bit_ext = true,
-    //         },
-    //         .message_type = .{
-    //             .general_bit_ext = true,
-    //             .validation_bit_ext = true,
-    //             .performance_bit_ext = true,
-    //         },
-    //         .pfn_user_callback = null,
-    //         .p_user_data = null,
-    //     }, null);
-    // }
+    if (builtin.mode == .Debug) {
+        self.debug_messenger = try self.instance.createDebugUtilsMessengerEXT(&.{
+            .message_severity = .{
+                .warning_bit_ext = true,
+                .error_bit_ext = true,
+            },
+            .message_type = .{
+                .general_bit_ext = true,
+                .validation_bit_ext = true,
+                .performance_bit_ext = true,
+            },
+            .pfn_user_callback = &debugUtilsMessengerCallback,
+            .p_user_data = null,
+        }, null);
+    }
 
     const sdl_surface: sdl3.vulkan.Surface = try .init(self.window, @ptrFromInt(@intFromEnum(self.instance.handle)), null);
     self.surface = @enumFromInt(@intFromPtr(sdl_surface.surface));
@@ -128,6 +128,17 @@ fn getInstanceExtensions() []const [*:0]const u8 {
         .Debug, .ReleaseSafe => &[_][*:0]const u8{vk.extensions.ext_debug_utils.name},
         else => &[_][*:0]const u8{},
     };
+}
+
+fn debugUtilsMessengerCallback(severity: vk.DebugUtilsMessageSeverityFlagsEXT, msg_type: vk.DebugUtilsMessageTypeFlagsEXT, callback_data: ?*const vk.DebugUtilsMessengerCallbackDataEXT, _: ?*anyopaque) callconv(.c) vk.Bool32 {
+    const severity_str = if (severity.verbose_bit_ext) "verbose" else if (severity.info_bit_ext) "info" else if (severity.warning_bit_ext) "warning" else if (severity.error_bit_ext) "error" else "unknown";
+
+    const type_str = if (msg_type.general_bit_ext) "general" else if (msg_type.validation_bit_ext) "validation" else if (msg_type.performance_bit_ext) "performance" else if (msg_type.device_address_binding_bit_ext) "device addr" else "unknown";
+
+    const message: [*c]const u8 = if (callback_data) |cb_data| cb_data.p_message else "NO MESSAGE!";
+    std.debug.print("[{s}][{s}]. Message:\n  {s}\n", .{ severity_str, type_str, message });
+
+    return .false;
 }
 
 fn initSwapchain() void {}
