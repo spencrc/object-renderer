c6bfd7ace9e55f3d6cf20e9a189f0bde88ffb785
spencer@laptop <145073726+spencrc@users.noreply.github.com>
2026-08-22 15:42:35 -0400

    feat: added dynamic rendering to pipeline

--- src/engine/graphics_context.zig
+++ src/engine/graphics_context.zig
@@ -7,6 +7,7 @@ const Vertex = @import("vertex.zig");
 
 const vert_spv align(@alignOf(u32)) = @embedFile("vertex_shader").*; // bytecode pointer is u32, hence the align
 const frag_spv align(@alignOf(u32)) = @embedFile("fragment_shader").*;
+const depth_format = vk.Format.d32_sfloat;
 
 const QueueFamilyIndices = struct {
     graphics_family_index: u32,
@@ -40,6 +41,7 @@ swap_image_views: []vk.ImageView,
 render_complete_semaphores: []vk.Semaphore,
 depth_image: vk.Image,
 depth_image_mem: vk.DeviceMemory,
+depth_image_view: vk.ImageView,
 
 render_pass: vk.RenderPass,
 
@@ -61,6 +63,7 @@ pub fn deinit(self: *GraphicsContext) void {
     // render pass
     self.device.destroyRenderPass(self.render_pass, null);
     // swapchain
+    self.device.destroyImageView(self.depth_image_view, null);
     self.device.freeMemory(self.depth_image_mem, null);
     self.device.destroyImage(self.depth_image, null);
     for (self.render_complete_semaphores) |s| self.device.destroySemaphore(s, null);
@@ -354,7 +357,7 @@ fn initSwapchain(self: *GraphicsContext, screen_width: usize, screen_height: usi
 
     const depth_image = try self.device.createImage(&.{
         .image_type = .@"2d",
-        .format = .d32_sfloat,
+        .format = depth_format,
         .extent = .{ .width = actual_extent.width, .height = actual_extent.height, .depth = 1 },
         .mip_levels = 1,
         .array_layers = 1,
@@ -367,7 +370,22 @@ fn initSwapchain(self: *GraphicsContext, screen_width: usize, screen_height: usi
     errdefer self.device.destroyImage(depth_image, null);
     const image_mem_reqs = self.device.getImageMemoryRequirements(depth_image);
     const image_mem = try self.allocate(image_mem_reqs, .{ .device_local_bit = true });
+    errdefer self.device.freeMemory(image_mem, null);
     try self.device.bindImageMemory(depth_image, image_mem, 0);
+
+    const depth_image_view = try self.device.createImageView(&.{
+        .image = depth_image,
+        .view_type = .@"2d",
+        .format = depth_format,
+        .components = .{ .r = .identity, .g = .identity, .b = .identity, .a = .identity },
+        .subresource_range = .{
+            .aspect_mask = .{ .depth_bit = true },
+            .base_mip_level = 0,
+            .level_count = 1,
+            .base_array_layer = 0,
+            .layer_count = 1,
+        },
+    }, null);
 
     self.surface_format = surface_format;
     self.actual_extent = actual_extent;
@@ -376,6 +394,7 @@ fn initSwapchain(self: *GraphicsContext, screen_width: usize, screen_height: usi
     self.render_complete_semaphores = render_complete_semaphores;
     self.depth_image = depth_image;
     self.depth_image_mem = image_mem;
+    self.depth_image_view = depth_image_view;
 }
 
 fn findSurfaceFormat(
@@ -514,13 +533,6 @@ fn initPipeline(self: *GraphicsContext) !void {
         },
     };
 
-    // enables changing the below at draw time, without recreating the pipeline
-    const dynamic_states = [_]vk.DynamicState{ .viewport, .scissor };
-    const dynamic_states_info = vk.PipelineDynamicStateCreateInfo{
-        .dynamic_state_count = dynamic_states.len,
-        .p_dynamic_states = &dynamic_states,
-    };
-
     const vertex_input_info = vk.PipelineVertexInputStateCreateInfo{
         .p_vertex_binding_descriptions = @ptrCast(&Vertex.binding_description),
         .vertex_binding_description_count = 1,
@@ -531,6 +543,34 @@ fn initPipeline(self: *GraphicsContext) !void {
     const input_assembly_info = vk.PipelineInputAssemblyStateCreateInfo{
         .topology = .triangle_list,
         .primitive_restart_enable = .false,
+    };
+
+    const depth_stencil_info = vk.PipelineDepthStencilStateCreateInfo{
+        .depth_test_enable = .true,
+        .depth_write_enable = .true,
+        .depth_compare_op = .less,
+        .stencil_test_enable = .false,
+        .depth_bounds_test_enable = .false,
+        .min_depth_bounds = 0,
+        .max_depth_bounds = 0,
+        .front = .{
+            .fail_op = .keep,
+            .pass_op = .keep,
+            .depth_fail_op = .keep,
+            .compare_op = .always,
+            .compare_mask = 0,
+            .reference = 0,
+            .write_mask = 0,
+        },
+        .back = .{
+            .fail_op = .keep,
+            .pass_op = .keep,
+            .depth_fail_op = .keep,
+            .compare_op = .always,
+            .compare_mask = 0,
+            .reference = 0,
+            .write_mask = 0,
+        },
     };
 
     const viewport_info = vk.PipelineViewportStateCreateInfo{
@@ -580,8 +620,25 @@ fn initPipeline(self: *GraphicsContext) !void {
         .blend_constants = [_]f32{ 0, 0, 0, 0 },
     };
 
+    // enables changing the below at draw time, without recreating the pipeline
+    const dynamic_states = [_]vk.DynamicState{ .viewport, .scissor };
+    const dynamic_states_info = vk.PipelineDynamicStateCreateInfo{
+        .dynamic_state_count = dynamic_states.len,
+        .p_dynamic_states = &dynamic_states,
+    };
+
+    // enable dynamic rendering
+    const render_info = vk.PipelineRenderingCreateInfo{
+        .color_attachment_count = 1,
+        .p_color_attachment_formats = &[_]vk.Format{self.surface_format.format},
+        .depth_attachment_format = depth_format,
+        .stencil_attachment_format = .undefined,
+        .view_mask = 0,
+    };
+
     const pipeline_info = vk.GraphicsPipelineCreateInfo{
         .stage_count = 2,
+        .p_next = &render_info,
         .p_stages = &shader_stages_info,
         .p_vertex_input_state = &vertex_input_info,
         .p_input_assembly_state = &input_assembly_info,
@@ -589,7 +646,7 @@ fn initPipeline(self: *GraphicsContext) !void {
         .p_viewport_state = &viewport_info,
         .p_rasterization_state = &rasterizer_info,
         .p_multisample_state = &multisampling_info,
-        .p_depth_stencil_state = null,
+        .p_depth_stencil_state = &depth_stencil_info,
         .p_color_blend_state = &color_blending_info,
         .p_dynamic_state = &dynamic_states_info,
         .layout = pipeline_layout,
